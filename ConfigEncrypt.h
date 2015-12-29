8ea031a967f6bedb10391b8b65e059bcaa19ce9b
Chris Punches <punches.chris@gmail.com>
2015-12-29 11:05:42 -0500

    Create ConfigEncrypt.h

---
+++ ConfigEncrypt.h
@@ -0,0 +1,9 @@
+#ifndef ConfCrypt
+#define ConfCrypt
+typedef char uint8_t;
+
+void decrypt(char *output, char *input);
+void encrypt(char *output, char *input);
+int demo( void );
+
+#endif //ConfCrypt
